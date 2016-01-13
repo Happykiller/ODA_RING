be81e712a69e72198402724b8d58f8140c08a16d
happykiller <fabrice.rosito@gmail.com>
2016-01-13 14:01:05 +0100

    update

--- server/class/RapportInterface.php
+++ server/class/RapportInterface.php
@@ -164,7 +164,7 @@ class RapportInterface extends OdaRestInterface {
             $params = new OdaPrepareReqSql();
             $params->sql = "SELECT DATE_FORMAT(a.`start`, '%Y-%m-%d') as 'startdate', DATE_FORMAT(a.`start`, '%Y-%m-%d') as 'enddate', WEEK(a.`start`) as 'week', MONTH(a.`start`) as 'month',
                 IF(d.`code`='default','BONITASOFT',d.`code`) as 'Customer', IF(a.`billable`,0,1) as 'Free', c.`code_user` as 'Consultant', 0 as 'days', a.`time` as 'hours',
-                f.`code` as 'deliverytype', e.`code` as 'Location', 'EMEA' as 'Region', a.`salesForceId` as 'deliverable', a.`googleICalUID` as 'EventID', 'na' as 'ConsultingID', CONCAT(a.`title`,', comment:',a.`cmt`) as 'Notes'
+                f.`code` as 'deliverytype', e.`code` as 'Location', 'EMEA' as 'Region', b.`salesForce` as 'deliverable', a.`googleICalUID` as 'EventID', 'na' as 'ConsultingID', CONCAT(a.`title`,', comment:',a.`cmt`) as 'Notes'
                 FROM `tab_events` a, `tab_accounts_items` b, `api_tab_utilisateurs` c, `tab_accounts` d, `tab_events_location` e, `tab_events_type` f
                 WHERE 1=1
                 AND a.`itemId` = b.`id`
